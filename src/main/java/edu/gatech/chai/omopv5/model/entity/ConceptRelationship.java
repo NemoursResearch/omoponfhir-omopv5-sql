ce372f5e35d79dc5afe98d98592b361c622e2caf
Bill Moyers <billmoyers@fastmail.com>
2024-05-20 19:17:13 -0400

    Enhancement: Working w/ value sets (not actually used anywhere except tests yet).

--- src/main/java/edu/gatech/chai/omopv5/model/entity/ConceptRelationship.java
+++ src/main/java/edu/gatech/chai/omopv5/model/entity/ConceptRelationship.java
@@ -30,12 +30,18 @@ public class ConceptRelationship extends BaseEntity {
 	 * 
 	 */
 
+	@Column(name="concept_id_1", nullable=false)
+	private Long conceptId1;
+
 	@JoinColumn(name="concept_id_1", referencedColumnName="concept_id", table="vocab.concept", nullable=false)
 	private Concept concept1;
-	
+
+	@Column(name="concept_id_2", nullable=false)
+    private Long conceptId2;
+
 	@JoinColumn(name="concept_id_2", referencedColumnName="concept_id", table="vocab.concept", nullable=false)
-    private Concept concept2;
-	
+	private Concept concept2;
+
 	@Column(name="relationship_id", nullable=false)
 	private String relationshipId;
 	
@@ -207,4 +213,19 @@ public class ConceptRelationship extends BaseEntity {
 		return "select * from concept_relationship ";
 	}
 
+	public Concept getConcept1() {
+		return concept1;
+	}
+
+	public void setConcept1(Concept concept1) {
+		this.concept1 = concept1;
+	}
+
+	public Concept getConcept2() {
+		return concept2;
+	}
+
+	public void setConcept2(Concept concept2) {
+		this.concept2 = concept2;
+	}
 }
